2bcfc383e844a7f88573a45d8a1c1ae06ac59956
outlandkarasu <outland.karasu@gmail.com>
2015-12-14 00:28:34 +0900

    issue #7 シェーダーでの描画を実装した。

--- source/app.d
+++ source/app.d
@@ -1,11 +1,13 @@
 import std.stdio;
 import std.string : toStringz;
+import std.algorithm : max;
 
 import derelict.sdl2.sdl;
 import derelict.opengl3.gl3;
 import derelict.assimp3.assimp;
 
 import ddoom.sdl;
+import ddoom.application : Application;
 
 /// 共有ライブラリのロード
 shared static this() {
@@ -18,6 +20,10 @@ enum {
     WINDOW_WIDTH = 1024, /// ウィンドウの幅
     WINDOW_HEIGHT = 768, /// ウィンドウの高さ
 }
+
+/// フレームレート
+enum FPS = 60.0;
+enum MS_PER_FRAME = cast(uint)(1000.0 / FPS);
 
 /// ウィンドウタイトル
 enum WINDOW_TITLE = "ddoom";
@@ -36,6 +42,9 @@ void main(string[] args) {
     // OpenGL ダブルバッファリング有効化
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
+    // OpenGL 最新バージョン使用
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
     // ウィンドウ生成
     auto window = enforceSDL(SDL_CreateWindow(
             toStringz(WINDOW_TITLE),
@@ -52,12 +61,20 @@ void main(string[] args) {
 
     // OpenGL3 ライブラリ再ロード
     DerelictGL3.reload();
+    debug writefln("OpenGL version: %s", DerelictGL3.loadedVersion);
+
+    // アプリケーション初期化
+    Application app = new Application();
+    scope(exit) app.exit();
 
     // メインループ
     for(bool loop = true; loop;) {
+        // フレーム開始時間
+        immutable beginTicks = SDL_GetTicks();
         for(SDL_Event event; SDL_PollEvent(&event);) {
             // キーボード押下で終了
             switch(event.type) {
+                case SDL_QUIT:
                 case SDL_KEYDOWN:
                     loop = false;
                     break;
@@ -65,6 +82,20 @@ void main(string[] args) {
                     break;
             }
         }
+
+        // 描画処理
+        try {
+            app.drawFrame();
+        } catch(Throwable t) {
+            stderr.writefln("error: %s", t);
+        }
+
+        // ウィンドウバッファ切り替え
+        SDL_GL_SwapWindow(window);
+
+        // 残り時間は待機
+        immutable elapse = SDL_GetTicks() - beginTicks;
+        SDL_Delay((elapse < MS_PER_FRAME) ? MS_PER_FRAME - elapse : 0);
     }
 }
 
